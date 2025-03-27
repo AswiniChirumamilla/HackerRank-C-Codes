324ac567a76557855b6bfd1a5877a5c875ed0257
ASWINI CHIRUMAMILLA <aswinichirumamilla16@gmail.com>
2025-03-27 20:55:13 +0530

    Create Pattern.c

---
+++ Pattern.c
@@ -0,0 +1,85 @@
+Print a pattern of numbers from  to  as shown below. Each of the numbers is separated by a single space.
+
+                            4 4 4 4 4 4 4  
+                            4 3 3 3 3 3 4   
+                            4 3 2 2 2 3 4   
+                            4 3 2 1 2 3 4   
+                            4 3 2 2 2 3 4   
+                            4 3 3 3 3 3 4   
+                            4 4 4 4 4 4 4   
+Input Format
+
+The input will contain a single integer .
+
+Constraints
+
+
+Sample Input 0
+
+2
+Sample Output 0
+
+2 2 2
+2 1 2
+2 2 2
+Sample Input 1
+
+5
+Sample Output 1
+
+5 5 5 5 5 5 5 5 5 
+5 4 4 4 4 4 4 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 2 1 2 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 4 4 4 4 4 4 5 
+5 5 5 5 5 5 5 5 5
+Sample Input 2
+
+7
+Sample Output 2
+
+7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 6 6 6 6 6 6 6 6 6 6 6 7 
+7 6 5 5 5 5 5 5 5 5 5 6 7 
+7 6 5 4 4 4 4 4 4 4 5 6 7 
+7 6 5 4 3 3 3 3 3 4 5 6 7 
+7 6 5 4 3 2 2 2 3 4 5 6 7 
+7 6 5 4 3 2 1 2 3 4 5 6 7 
+7 6 5 4 3 2 2 2 3 4 5 6 7 
+7 6 5 4 3 3 3 3 3 4 5 6 7 
+7 6 5 4 4 4 4 4 4 4 5 6 7 
+7 6 5 5 5 5 5 5 5 5 5 6 7 
+7 6 6 6 6 6 6 6 6 6 6 6 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 
+
+
+
+
+#####################CODE#######################3
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+  	// Complete the code to print the pattern.
+    int size=2*n-1;
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            int min_dist=i<size-i-1 ?i:size-i-1;
+            min_dist=min_dist < j ? min_dist:j;
+            min_dist=min_dist<size - j - 1 ? min_dist : size-j-1;
+            printf("%d ",n-min_dist);
+        }
+        printf("\n");
+    }
+    return 0;
+}
