910cc554b19c5b4129119f4ab51c74d05bca47a4
ASWINI CHIRUMAMILLA <aswinichirumamilla16@gmail.com>
2025-04-16 17:02:25 +0530

    Update PrintingTokens.c

--- PrintingTokens.c
+++ PrintingTokens.c
@@ -3,10 +3,7 @@ Given a sentence, , print each word of the sentence in a new line.
 Input Format
 
 The first and only line contains a sentence, .
-
 Constraints
-
-
 Output Format
 
 Print each word of the sentence in a new line.
