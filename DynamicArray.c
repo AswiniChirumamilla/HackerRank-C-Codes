d0123d41ae52768272e33c8b50eade1168767012
ASWINI CHIRUMAMILLA <aswinichirumamilla16@gmail.com>
2025-04-16 17:02:49 +0530

    Update DynamicArray.c

--- DynamicArray.c
+++ DynamicArray.c
@@ -1,11 +1,7 @@
 Snow Howler is the librarian at the central library of the city of HuskyLand. He must handle requests which come in the following forms:
-
 1 x y : Insert a book with  pages at the end of the  shelf.
-
 2 x y : Print the number of pages in the  book on the  shelf.
-
 3 x : Print the number of books on the  shelf.
-
 Snow Howler has got an assistant, Oshie, provided by the Department of Education. Although inexperienced, Oshie can handle all of the queries of types 2 and 3.
 
 Help Snow Howler deal with all the queries of type 1.
